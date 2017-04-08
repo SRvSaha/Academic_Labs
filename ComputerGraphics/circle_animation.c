1c399eeb2d676a92b729292c7475ca804c0c95eb
SRvSaha <contact.srvsaha@gmail.com>
2017-04-08 12:27:08 +0530

    Window

---
+++ ComputerGraphics/circle_animation.c
@@ -0,0 +1,31 @@
+/*
+@author 	: SRvSaha
+Date 		: 22-03-2017
+Description : Animation of Circle
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <graphics.h>
+#include <math.h>
+
+int main(){
+	int gd = DETECT, gm;
+	initgraph(&gd, &gm, NULL);
+	int r = 40;
+	int i,x, y;
+	while(1){
+		x = rand()%(getmaxx());
+		y = rand()%(getmaxy());
+		if(x <= r || y <= r || x >= getmaxx()-r || y >= getmaxy()-r){
+			circle(getmaxx()/2,getmaxy()/2,r);
+		}
+		else{
+			circle(x,y,r);
+		}
+		delay(1000);
+		cleardevice();
+	}
+	return 0;
+}
